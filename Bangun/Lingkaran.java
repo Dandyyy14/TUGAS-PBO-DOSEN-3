63dee1d3fedaadd64ed0078a11ef0d421baab8b5
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-25 11:41:21 +0700

    Create Lingkaran.java

---
+++ Bangun/Lingkaran.java
@@ -0,0 +1,9 @@
+public class Lingkaran {
+    public double keliling(double r) {
+        return 2 * Math.PI * r;
+    }
+
+    public double luas(double r) {
+        return Math.PI * r * r;
+    }
+}

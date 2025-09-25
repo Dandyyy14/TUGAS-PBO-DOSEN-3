46a4d93028861bfe3d681062c5f36244deb37e93
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-25 11:13:02 +0700

    Create Matematika.java

---
+++ Matematika.java
@@ -0,0 +1,37 @@
+// Class dasar
+public class Matematika {
+    // Overloading pertambahan
+    public int pertambahan(int a, int b) {
+        return a + b;
+    }
+
+    public double pertambahan(double a, double b) {
+        return a + b;
+    }
+
+    public double pertambahan(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public int pengurangan(int a, int b) {
+        return a - b;
+    }
+
+    public double pengurangan(double a, double b) {
+        return a - b;
+    }
+
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+
+    // Pembagian int
+    public int pembagian(int a, int b) {
+        return a / b;
+    }
+
+    // Pembagian double
+    public double pembagian(double a, double b) {
+        return a / b;
+    }
+}

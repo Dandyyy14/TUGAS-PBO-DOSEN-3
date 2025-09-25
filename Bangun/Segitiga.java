e405a2c0c2cc954d158cee662702492bf462e484
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-25 11:40:45 +0700

    Create Segitiga.java

---
+++ Bangun/Segitiga.java
@@ -0,0 +1,9 @@
+public class Segitiga {
+    public double keliling(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public double luas(double alas, double tinggi) {
+        return 0.5 * alas * tinggi;
+    }
+}

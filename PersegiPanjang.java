132869cb651007b35c0f9992ac7a7d7e1946d32b
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-25 11:25:59 +0700

    Create PersegiPanjang.java

---
+++ PersegiPanjang.java
@@ -0,0 +1,9 @@
+public class PersegiPanjang {
+    public double keliling(double p, double l) {
+        return 2 * (p + l);
+    }
+
+    public double luas(double p, double l) {
+        return p * l;
+    }
+}
